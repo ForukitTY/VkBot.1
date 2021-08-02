d1db227804c8c99f03c694119d4d395f20e501da
Mukhamed <cool.gedgafov@mail.ru>
2021-08-03 02:08:20 +0300

    убралTime

--- noth.py
+++ noth.py
@@ -32,9 +32,6 @@ class VkBot:
         # Привет
         if message.upper() == self._COMMANDS[0]:
             return f"Уи махуэ фlыуэ, {self._USERNAME}!"
-        if message.upper() == self._COMMANDS[3]:            
-            vk.method('messages.send', {'user_id': event.user_id, 'attachment': 'photo-174309237_457256559', 'random_id': 0})
-            return " "
 
         # Погода
         if message.upper().split()[0]==self._COMMANDS[1]:
@@ -51,23 +48,12 @@ class VkBot:
             return f'Гъуэгу махуэ, {self._USERNAME}'
         
         #Parser
-        elif message.upper() == self._COMMANDS[4]:
+        elif message.upper() == self._COMMANDS[3]:
              return self.pars()
 
         else: 
             return "могу только погоду сказать или попрощаться. \nЧтобы узнать погоду напиши 'погода'"
-#Time
-    def _get_time(self):
-        request = requests.get("https://my-calend.ru/date-and-time-today")
-        if request.status_code == 200:
-            print('Success! time req')
-        else:
-            print("casino time req")
-        b = BeautifulSoup(request.text, "html.parser")
-        res2=self._clean_all_tag_from_str(b.find_all("h2"))
-        
-        res2=res2.replace('В', '\nB')
-        return res2
+
 #Weather
     def _get_weather(self, city):
         request = requests.get("https://sinoptik.com.ru/погода-" + city.lower())
@@ -80,7 +66,7 @@ class VkBot:
         min_and_maxWeath = min_and_maxWeath.replace('мин.', 'Минимум днем')
         currentWeather = self._clean_all_tag_from_str(b.select('.table__col.current .table__felt')[0])
         return f'Сегодня в {city.title()}: {min_and_maxWeath} \nВ данный момент {currentWeather}'
-
+#parsing
     def pars(self):
         outputSum=""
         
@@ -123,7 +109,7 @@ class VkBot:
             self._USER_CITY=False
 
         self._USERNAME = vk.method('users.get',{'user_ids':user_id})[0]['first_name']
-        self._COMMANDS = ["ПРИВЕТ", "ПОГОДА", "ПОКА", "ЗАПРЕЩЁНКА", "СТАРТ"]
+        self._COMMANDS = ["ПРИВЕТ", "ПОГОДА", "ПОКА", "СТАРТ"]
         print(f'Указанный город для id {user_id}: ', self._USER_CITY)
 
 print("Server started")
